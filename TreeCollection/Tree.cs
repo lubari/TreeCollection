241d5f1aff7acbbd1bf515311e0f78fbc938aa0a
Ana <analubari@gmail.com>
2023-05-12 00:15:22 -0300

    tree collection

--- TreeCollection/Tree.cs
+++ TreeCollection/Tree.cs
@@ -3,42 +3,134 @@
 namespace TreeCollection
 {
     public class Tree<T> : IEnumerable<T> 
-    {
-        private Node root;
+    {private class TreeNode<T>
+        {
+            public T Value;
+            public TreeNode<T> Left;
+            public TreeNode<T> Right;
+        }
+
+        private TreeNode<T> root;
+        private bool isReversed;
+
         public Tree(bool isReversed = false) 
         {
-            throw new NotImplementedException();
-        }
-        
-        public void CreateTree(){
-            this.root = new Node();
+            root = null;
+            this.isReversed = isReversed;
         }
 
         public void Add(T newElement)
         {
-            if (this.root == null)
+            if (root == null) 
             {
-                this.root = new Node();
-                this.root.value = newElement;
+                root = new TreeNode<T> { Value = newElement };
+            } 
+            else 
+            {
+                TreeNode<T> currentNode = root;
+                while (true) 
+                {
+                    int comparisonResult = Comparer<T>.Default.Compare(newElement, currentNode.Value);
+                    if (comparisonResult == 0) 
+                    {
+                        throw new ArgumentException("Element already exists in the tree.");
+                    } 
+                    else if (comparisonResult < 0) 
+                    {
+                        if (currentNode.Left == null) 
+                        {
+                            currentNode.Left = new TreeNode<T> { Value = newElement };
+                            break;
+                        } 
+                        else 
+                        {
+                            currentNode = currentNode.Left;
+                        }
+                    } 
+                    else 
+                    {
+                        if (currentNode.Right == null) 
+                        {
+                            currentNode.Right = new TreeNode<T> { Value = newElement };
+                            break;
+                        } 
+                        else 
+                        {
+                            currentNode = currentNode.Right;
+                        }
+                    }
+                }
             }
-            
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            if (root == null) 
+            {
+                yield break;
+            }
+
+            if (isReversed) 
+            {
+                foreach (T value in ReverseInOrderTraversal(root)) 
+                {
+                    yield return value;
+                }
+            } 
+            else 
+            {
+                foreach (T value in InOrderTraversal(root)) 
+                {
+                    yield return value;
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
 
-        private class Node
-        {   
-            public Object? value;
-            public Object? right;
-            public Object? left;
+        private IEnumerable<T> InOrderTraversal(TreeNode<T> node)
+        {
+            if (node.Left != null) 
+            {
+                foreach (T value in InOrderTraversal(node.Left)) 
+                {
+                    yield return value;
+                }
+            }
+
+            yield return node.Value;
+
+            if (node.Right != null) 
+            {
+                foreach (T value in InOrderTraversal(node.Right)) 
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        private IEnumerable<T> ReverseInOrderTraversal(TreeNode<T> node)
+        {
+            if (node.Right != null) 
+            {
+                foreach (T value in ReverseInOrderTraversal(node.Right)) 
+                {
+                    yield return value;
+                }
+            }
+
+            yield return node.Value;
+
+            if (node.Left != null) 
+            {
+                foreach (T value in ReverseInOrderTraversal(node.Left)) 
+                {
+                    yield return value;
+                }
+            }
         }
     }
 }
